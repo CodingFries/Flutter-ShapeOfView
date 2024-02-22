c01db997ec95ddaca8549816f58e843be5551bdd
Awais Amjed <awaisamjed171@gmail.com>
2024-02-22 16:52:15 +0500

    Upgrade example project

--- example/android/app/src/main/kotlin/com/github/florent37/flutter_shape_of_view_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/github/florent37/flutter_shape_of_view_example/MainActivity.kt
@@ -1,13 +1,6 @@
 package com.github.florent37.flutter_shape_of_view_example
 
-import android.os.Bundle
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
+import io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+
 }
